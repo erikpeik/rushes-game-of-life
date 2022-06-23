d3bab620895e40cf69c3a7627a05c62334fb16da
Erik Mende <emende@student.hive.fi>
2022-06-23 20:47:11 +0300

    Pulled everything from Gitlab what we used when doing this

---
+++ game_of_life.go
@@ -0,0 +1,182 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"runtime/debug"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func print_array(array [][]int, height int, width int) {
+	var sb strings.Builder
+
+	for i := 0; i < height; i++ {
+		for j := 0; j < width; j++ {
+			if array[i][j] == 1 {
+				sb.WriteString("X")
+			} else {
+				sb.WriteString(".")
+			}
+		}
+		if i != height-1 {
+			sb.WriteString("\n")
+		}
+	}
+	fmt.Println(sb.String())
+}
+
+func is_in_bound(x int, y int, width int, height int, arr [][]int) bool {
+	if x >= 0 && y >= 0 && x < height && y < width {
+		return true
+	} else {
+		return false
+	}
+}
+
+func insert(cell int, x int, y int, width int, height int, arr [][]int) {
+	if is_in_bound(x, y, width, height, arr) {
+		arr[y][x] = cell
+	} else {
+		print("out of bound")
+	}
+}
+
+func populate_map(arr [][]int, width int, height int) {
+	for i := 0; i < height; i++ {
+		for j := 0; j < width; j++ {
+			rand.Seed(time.Now().UnixNano())
+			arr[i][j] = rand.Intn(2)
+		}
+	}
+}
+
+func check_neighbour(x int, y int, width int, height int, arr [][]int) int {
+	var num_of_live_neighbour = 0
+	if is_in_bound(x, y+1, width, height, arr) && arr[x][y+1] == 1 {
+		num_of_live_neighbour += 1
+	}
+	if is_in_bound(x, y-1, width, height, arr) && arr[x][y-1] == 1 {
+		num_of_live_neighbour += 1
+	}
+	if is_in_bound(x-1, y, width, height, arr) && arr[x-1][y] == 1 {
+		num_of_live_neighbour += 1
+	}
+	if is_in_bound(x+1, y, width, height, arr) && arr[x+1][y] == 1 {
+		num_of_live_neighbour += 1
+	}
+	if is_in_bound(x+1, y+1, width, height, arr) && arr[x+1][y+1] == 1 {
+		num_of_live_neighbour += 1
+	}
+	if is_in_bound(x-1, y-1, width, height, arr) && arr[x-1][y-1] == 1 {
+		num_of_live_neighbour += 1
+	}
+	if is_in_bound(x-1, y+1, width, height, arr) && arr[x-1][y+1] == 1 {
+		num_of_live_neighbour += 1
+	}
+	if is_in_bound(x+1, y-1, width, height, arr) && arr[x+1][y-1] == 1 {
+		num_of_live_neighbour += 1
+	}
+	return num_of_live_neighbour
+}
+
+func check_rules(x int, y int, width int, height int, arr [][]int) {
+	var num_of_live_neighbour = check_neighbour(x, y, width, height, arr)
+
+	if num_of_live_neighbour == 3 && arr[x][y] == 0 {
+		par := []int{x, y, 1}
+		changes = append(changes, par)
+	}
+
+	if num_of_live_neighbour < 2 && arr[x][y] == 1 {
+		par := []int{x, y, 0}
+		changes = append(changes, par)
+	}
+
+	if num_of_live_neighbour > 3 && arr[x][y] == 1 {
+		par := []int{x, y, 0}
+		changes = append(changes, par)
+	}
+}
+
+func check_if_file_exist(err error) bool {
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	return (err != nil)
+}
+
+func full_check(width int, height int, arr [][]int) {
+	for i := 0; i < height; i++ {
+		for j := 0; j < width; j++ {
+			check_rules(i, j, width, height, arr)
+		}
+	}
+}
+
+var changes [][]int
+
+func main() {
+	debug.SetGCPercent(-1)
+
+	if len(os.Args) != 3 {
+		println("Usage: ./life initial_state iterations")
+		os.Exit(1)
+	}
+	initial_state := os.Args[1]
+	var file, err = os.OpenFile(initial_state, os.O_RDWR, 0644)
+	if check_if_file_exist(err) {
+		return
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+	var array [][]int
+	var height = 0
+	var width = 0
+	for scanner.Scan() {
+		height += 1
+		line := scanner.Text()
+		if len(line) > width {
+			width = len(line)
+		}
+		var int_line []int
+
+		for c := 0; c < len(line); c++ {
+			if line[c] == 46 {
+				int_line = append(int_line, 0)
+			} else if line[c] == 88 {
+				int_line = append(int_line, 1)
+			}
+		}
+		array = append(array, int_line)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	iterations, err := strconv.Atoi(os.Args[2])
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	for g := 0; g < iterations; g++ {
+		changes = nil
+		full_check(width, height, array)
+		if len(changes) == 0 {
+			break
+		}
+		for i := 0; i < len(changes); i++ {
+			array[changes[i][0]][changes[i][1]] = changes[i][2]
+		}
+	}
+	print_array(array, height, width)
+}
---
+++ game_of_life.py
@@ -0,0 +1,111 @@
+import sys
+import numpy as np
+
+from os import system
+clear = lambda: system('clear')
+from random import randint
+
+def print_array(arr_2d):
+	for i in arr_2d:
+		for j in i:
+			if (j == 1):
+				print("X", end ="")
+			else:
+				print(".", end ="")
+		print()
+
+def is_in_bound(x, y, width, height, arr):
+	if (x >= 0 and y >= 0 and x < height and y < width):
+		return True
+	else:
+		return False
+
+def check_neigbour(x, y, width, height, arr):
+	num_of_live_neighbour = 0
+
+	if (is_in_bound(x, y+1, width, height, arr) and arr[x][y+1] == 1):
+		num_of_live_neighbour += 1
+	if (is_in_bound(x, y-1, width, height, arr) and arr[x][y-1] == 1):
+		num_of_live_neighbour += 1
+	if (is_in_bound(x-1, y, width, height, arr) and arr[x-1][y] == 1):
+		num_of_live_neighbour += 1
+	if (is_in_bound(x+1, y, width, height, arr) and arr[x+1][y] == 1):
+		num_of_live_neighbour += 1
+	if (is_in_bound(x+1, y+1, width, height, arr) and arr[x+1][y+1] == 1):
+		num_of_live_neighbour += 1
+	if (is_in_bound(x-1, y-1, width, height, arr) and arr[x-1][y-1] == 1):
+		num_of_live_neighbour += 1
+	if (is_in_bound(x-1, y+1, width, height, arr) and arr[x-1][y+1] == 1):
+		num_of_live_neighbour += 1
+	if (is_in_bound(x+1, y-1, width, height, arr) and arr[x+1][y-1] == 1):
+		num_of_live_neighbour += 1
+	return num_of_live_neighbour
+
+def full_check(width, heigth, arr):
+	changes = []
+	for i in range(heigth):
+		for j in range(width):
+			round = check_rules(i, j, width, heigth, arr)
+			if (len(round)):
+				changes.append(round)
+	return changes
+
+# Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+# Any live cell with two or three live neighbours lives on to the next generation.
+# Any live cell with more than three live neighbours dies, as if by overpopulation.
+# Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+
+def check_rules(x: int, y: int, width, height, map):
+	num_of_live_neigbours = check_neigbour(x, y, width, height, map)
+	changes = []
+	#born rule
+	if (num_of_live_neigbours == 3 and map[x][y] == 0):
+		par = [x, y, 1]
+		changes.append(par)
+
+	#underpopulation rule
+	if (num_of_live_neigbours < 2 and map[x][y] == 1):
+		par = [x, y, 0]
+		changes.append(par)
+
+	#overpopulate rule
+	if (num_of_live_neigbours > 3 and map[x][y] == 1):
+		par = [x, y, 0]
+		changes.append(par)
+	return changes
+
+def main():
+	if len(sys.argv) != 3:
+		print("Usage: python3 game_of_life.py inital_state iterations")
+		return
+	array = []
+	width = 0
+	heigth = 0
+	initial_state = sys.argv[1]
+	fd = open(initial_state, "r")
+	lines = fd.readlines()
+	for line in lines:
+		if len(line) - 1 > width:
+			width = len(line) - 1
+		int_line = []
+		for c in line:
+			if c == '.':
+				int_line.append(0)
+			elif c == 'X':
+				int_line.append(1)
+		array.append(int_line)
+		heigth += 1
+
+	iterations = int(sys.argv[2])
+	for g in range(iterations):
+		changes = []
+		changes = full_check(width, heigth, array)
+		for i in range(len(changes)):
+			changes[i] = changes[i][0]
+		if len(changes) == 0:
+			break
+		for i in range(len(changes)):
+			array[changes[i][0]][changes[i][1]] = changes[i][2]
+	print_array(array)
+
+main()
